a9c488f86c4a75eb9f7fc3e3a171696d851db5a7
Jan Milota <jan.milota@vivnetworks.com>
2018-02-14 13:29:29 +0100

    Optional API host URI configuration

--- src/SparkPostDotNet/SparkPostOptions.cs
+++ src/SparkPostDotNet/SparkPostOptions.cs
@@ -3,5 +3,10 @@
     public class SparkPostOptions
     {
         public string ApiKey { get; set; }
+
+        /// <summary>
+        /// An optional host uri. If none is provided, the default of 'https://api.sparkpost.com/api/v1/transmissions' is used.
+        /// </summary>
+        public string ApiHostUri { get; set; }
     }
 }
